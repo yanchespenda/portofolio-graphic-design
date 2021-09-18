d5d60a428b3ea5aebf2b23f06e35d1a5a28a4536
Alfian Rikzandi <denydirgantara94@gmail.com>
2021-09-18 20:36:27 +0700

    chore: add some function in console work add

--- pages/console/works/add.tsx
+++ pages/console/works/add.tsx
@@ -4,12 +4,16 @@ import dynamic from 'next/dynamic'
 import { Component, Fragment, useEffect, useRef, useState } from "react"
 import Dropzone, { DropEvent, FileRejection } from 'react-dropzone'
 import { Formik } from 'formik'
-import { convertToRaw, EditorState } from 'draft-js'
+import { convertToRaw, EditorState, Modifier } from 'draft-js'
 
 import ConsoleHeader from '../../../components/console/Header'
 import { firebaseApp } from "../../../firebase/init"
 
-import { Editor } from 'react-draft-wysiwyg'
+import { EditorProps } from 'react-draft-wysiwyg'
+const Editor = dynamic<EditorProps>(
+  () => import('react-draft-wysiwyg').then(mod => mod.Editor),
+  { ssr: false }
+)
 
 import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 
@@ -34,6 +38,8 @@ class ConsoleWorksAdd extends Component<IProps, IState> {
       imageCoverPreview: null,
       contentEditor: EditorState.createEmpty(),
     }
+
+    this.contentEditorRefresh = this.contentEditorRefresh.bind(this)
   }
 
   async validateAuth() {
@@ -50,7 +56,8 @@ class ConsoleWorksAdd extends Component<IProps, IState> {
   }
 
   contentEditorRefresh(editorState: EditorState) {
-    this.setState({ contentEditor: editorState })
+    if (this.state.contentEditor)
+      this.setState({ contentEditor: editorState })
   }
 
   handleFile(file: File) {
@@ -173,9 +180,182 @@ class ConsoleWorksAdd extends Component<IProps, IState> {
                       wrapperClassName="demo-wrapper"
                       editorClassName="demo-editor"
                       onEditorStateChange={this.contentEditorRefresh}
-                      toolbar={{
 
-                      }}
+                      toolbar={
+                        {
+                          options: ['inline', 'blockType', 'fontSize', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji'],
+                          inline: {
+                            inDropdown: false,
+                            className: undefined,
+                            component: undefined,
+                            dropdownClassName: undefined,
+                            options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace', 'superscript', 'subscript'],
+                            bold: {
+                              icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjRweCIgdmlld0JveD0iMCAwIDI0IDI0IiB3aWR0aD0iMjRweCIgZmlsbD0iIzAwMDAwMCI+PHBhdGggZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0ibm9uZSIvPjxwYXRoIGQ9Ik0xNS42IDEwLjc5Yy45Ny0uNjcgMS42NS0xLjc3IDEuNjUtMi43OSAwLTIuMjYtMS43NS00LTQtNEg3djE0aDcuMDRjMi4wOSAwIDMuNzEtMS43IDMuNzEtMy43OSAwLTEuNTItLjg2LTIuODItMi4xNS0zLjQyek0xMCA2LjVoM2MuODMgMCAxLjUuNjcgMS41IDEuNXMtLjY3IDEuNS0xLjUgMS41aC0zdi0zem0zLjUgOUgxMHYtM2gzLjVjLjgzIDAgMS41LjY3IDEuNSAxLjVzLS42NyAxLjUtMS41IDEuNXoiLz48L3N2Zz4=',
+                              className: undefined },
+                            italic: {
+                              // icon: 'italic',
+                              className: undefined },
+                            underline: {
+                              // icon: 'underline',
+                              className: undefined },
+                            strikethrough: {
+                              // icon: 'strikethrough',
+                              className: undefined },
+                            monospace: {
+                              // icon: 'monospace',
+                              className: undefined },
+                            superscript: {
+                              // icon: 'superscript',
+                              className: undefined },
+                            subscript: {
+                              // icon: 'subscript',
+                              className: undefined },
+                          },
+                          blockType: {
+                            inDropdown: true,
+                            options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote', 'Code'],
+                            className: undefined,
+                            component: undefined,
+                            dropdownClassName: undefined,
+                          },
+                          fontSize: {
+                            // icon: 'fontSize',
+                            options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
+                            className: undefined,
+                            component: undefined,
+                            dropdownClassName: undefined,
+                          },
+                          list: {
+                            inDropdown: false,
+                            className: undefined,
+                            component: undefined,
+                            dropdownClassName: undefined,
+                            options: ['unordered', 'ordered', 'indent', 'outdent'],
+                            unordered: { 
+                              // icon: 'unordered', 
+                              className: undefined },
+                            ordered: { 
+                              // icon: 'ordered', 
+                              className: undefined },
+                            indent: { 
+                              // icon: 'indent', 
+                              className: undefined },
+                            outdent: { 
+                              // icon: 'outdent', 
+                              className: undefined },
+                          },
+                          textAlign: {
+                            inDropdown: false,
+                            className: undefined,
+                            component: undefined,
+                            dropdownClassName: undefined,
+                            options: ['left', 'center', 'right', 'justify'],
+                            left: { 
+                              // icon: 'left', 
+                              className: undefined },
+                            center: { 
+                              // icon: 'center', 
+                              className: undefined },
+                            right: { 
+                              // icon: 'right', 
+                              className: undefined },
+                            justify: { 
+                              // icon: 'justify', 
+                              className: undefined },
+                          },
+                          colorPicker: {
+                            // icon: 'color',
+                            className: undefined,
+                            component: undefined,
+                            popupClassName: undefined,
+                            colors: ['rgb(97,189,109)', 'rgb(26,188,156)', 'rgb(84,172,210)', 'rgb(44,130,201)',
+                              'rgb(147,101,184)', 'rgb(71,85,119)', 'rgb(204,204,204)', 'rgb(65,168,95)', 'rgb(0,168,133)',
+                              'rgb(61,142,185)', 'rgb(41,105,176)', 'rgb(85,57,130)', 'rgb(40,50,78)', 'rgb(0,0,0)',
+                              'rgb(247,218,100)', 'rgb(251,160,38)', 'rgb(235,107,86)', 'rgb(226,80,65)', 'rgb(163,143,132)',
+                              'rgb(239,239,239)', 'rgb(255,255,255)', 'rgb(250,197,28)', 'rgb(243,121,52)', 'rgb(209,72,65)',
+                              'rgb(184,49,47)', 'rgb(124,112,107)', 'rgb(209,213,216)'],
+                          },
+                          link: {
+                            inDropdown: false,
+                            className: undefined,
+                            component: undefined,
+                            popupClassName: undefined,
+                            dropdownClassName: undefined,
+                            showOpenOptionOnHover: true,
+                            defaultTargetOption: '_self',
+                            options: ['link', 'unlink'],
+                            link: { 
+                              // icon: 'link', 
+                              className: undefined },
+                            unlink: { 
+                              // icon: 'unlink', 
+                              className: undefined },
+                            linkCallback: undefined
+                          },
+                          emoji: {
+                            // icon: 'emoji',
+                            className: undefined,
+                            component: undefined,
+                            popupClassName: undefined,
+                            emojis: [
+                              '😀', '😁', '😂', '😃', '😉', '😋', '😎', '😍', '😗', '🤗', '🤔', '😣', '😫', '😴', '😌', '🤓',
+                              '😛', '😜', '😠', '😇', '😷', '😈', '👻', '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '🙈',
+                              '🙉', '🙊', '👼', '👮', '🕵', '💂', '👳', '🎅', '👸', '👰', '👲', '🙍', '🙇', '🚶', '🏃', '💃',
+                              '⛷', '🏂', '🏌', '🏄', '🚣', '🏊', '⛹', '🏋', '🚴', '👫', '💪', '👈', '👉', '👉', '👆', '🖕',
+                              '👇', '🖖', '🤘', '🖐', '👌', '👍', '👎', '✊', '👊', '👏', '🙌', '🙏', '🐵', '🐶', '🐇', '🐥',
+                              '🐸', '🐌', '🐛', '🐜', '🐝', '🍉', '🍄', '🍔', '🍤', '🍨', '🍪', '🎂', '🍰', '🍾', '🍷', '🍸',
+                              '🍺', '🌍', '🚑', '⏰', '🌙', '🌝', '🌞', '⭐', '🌟', '🌠', '🌨', '🌩', '⛄', '🔥', '🎄', '🎈',
+                              '🎉', '🎊', '🎁', '🎗', '🏀', '🏈', '🎲', '🔇', '🔈', '📣', '🔔', '🎵', '🎷', '💰', '🖊', '📅',
+                              '✅', '❎', '💯',
+                            ],
+                          },
+                          embedded: {
+                            // icon: 'embedded',
+                            className: undefined,
+                            component: undefined,
+                            popupClassName: undefined,
+                            embedCallback: undefined,
+                            defaultSize: {
+                              height: 'auto',
+                              width: 'auto',
+                            },
+                          },
+                          image: {
+                            // icon: 'image',
+                            className: undefined,
+                            component: undefined,
+                            popupClassName: undefined,
+                            urlEnabled: true,
+                            uploadEnabled: true,
+                            alignmentEnabled: true,
+                            uploadCallback: undefined,
+                            previewImage: false,
+                            inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
+                            alt: { present: false, mandatory: false },
+                            defaultSize: {
+                              height: 'auto',
+                              width: 'auto',
+                            },
+                          },
+                          remove: { 
+                            // icon: 'eraser', 
+                            className: undefined, component: undefined },
+                          history: {
+                            inDropdown: false,
+                            className: undefined,
+                            component: undefined,
+                            dropdownClassName: undefined,
+                            options: ['undo', 'redo'],
+                            undo: { 
+                              // icon: 'undo', 
+                              className: undefined },
+                            redo: { 
+                              // icon: 'redo', 
+                              className: undefined },
+                          },
+                        }
+                      }
                     />
                   </div>
                   {/* End Step 4: Content */}
@@ -205,6 +385,10 @@ class ConsoleWorksAdd extends Component<IProps, IState> {
             </Formik>
 
           </div>
+        </div>
+        
+        <div className="hidden overflow-hidden h-0 w-0">
+          <svg id="svg_bold" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.6 10.79c.97-.67 1.65-1.77 1.65-2.79 0-2.26-1.75-4-4-4H7v14h7.04c2.09 0 3.71-1.7 3.71-3.79 0-1.52-.86-2.82-2.15-3.42zM10 6.5h3c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5h-3v-3zm3.5 9H10v-3h3.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5z"/></svg>
         </div>
       </Fragment>
     )
